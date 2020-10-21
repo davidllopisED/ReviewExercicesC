4cbb9a948d1c8242dd45ff6d9ba771e86f4ae6e3
davidllopisED <davidllopis@paucasesnovescifp.cat>
2020-10-21 19:30:40 +0200

    In this exercise I have created the challenge 1 that was asked of us, using a for-each.

---
+++ Challenge1/PersonModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1
+{
+    class PersonModel
+    {
+        string firstName, lastName;
+
+        public PersonModel(string Name1, string Name2)
+        {
+            FirstName = Name1;
+            LastName = Name2;
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+    }
+}
---
+++ Challenge1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> names = new List<String>();
+            names.Add("John");
+            names.Add("Mary");
+            names.Add("Sue");
+            names.Add("Greg");
+            names.Add("Yolanda");
+            names.Add("Jose");
+            names.Add("Bill");
+            names.Add("Gates");
+            foreach (string name in names)
+            {
+                Console.WriteLine("Hello Ms/Sir " + name);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Please press any key to continue");
+            Console.ReadKey();
+            Console.WriteLine("");
+
+            List<PersonModel> namesSurnames = new List<PersonModel>();
+            namesSurnames.Add(new PersonModel("Tim", "Corey"));
+            namesSurnames.Add(new PersonModel("Bill", "McOy"));
+            namesSurnames.Add(new PersonModel("Mary", "Jones"));
+            namesSurnames.Add(new PersonModel("Steve", "Works"));
+            namesSurnames.Add(new PersonModel("Sue", "Smith"));
+            foreach (PersonModel person in namesSurnames)
+            {
+                Console.WriteLine("Hello Ms/Sir " + person.FirstName + " " + person.LastName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
